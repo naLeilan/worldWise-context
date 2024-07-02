96a9ae4d03ff27caf438c73523e5e5681192fa88
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-02 17:24:58 +0300

    added message to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
+import Message from "./Message";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -24,6 +25,8 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(
     function () {
@@ -32,20 +35,24 @@ function Form() {
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
+          setGeocodingError("");
 
           const res = await fetch(
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
 
-          if (!data.countryCode)
+          if (data.countryCode === "NO" || !data.countryCode)
             throw new Error(
               "That doesn't seem to be a city. Click somewhere else 😉"
             );
 
           setCityName(data.city || data.locality || "");
           setCountry(data.countryName);
+          console.log(data);
+          setEmoji(data.countryCode);
         } catch (err) {
+          setGeocodingError(err.message);
         } finally {
           setIsLoadingGeocoding(false);
         }
@@ -55,6 +62,7 @@ function Form() {
     [lat, lng]
   );
 
+  if (geocodingError) return <Message message={geocodingError} />;
   return (
     <form className={styles.form}>
       <div className={styles.row}>
