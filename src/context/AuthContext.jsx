809af2de828f8556047807c4081fd734c059c947
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-03 17:53:05 +0300

    added reducer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,37 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispach] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, pass) {}
+
+  function logout() {}
+
   return <AuthContext.Provider>{children} </AuthContext.Provider>;
 }
 
