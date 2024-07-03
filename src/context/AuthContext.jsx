d4a915501bf8f9b8b506eb233a9e6aa96208ec32
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-07-03 17:44:17 +0300

    added AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  return <AuthContext.Provider>{children} </AuthContext.Provider>;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used out of AuthProvider");
+  return context;
+}
+
+export { AuthProvider };
